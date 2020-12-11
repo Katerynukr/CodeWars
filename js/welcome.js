79301b8e06df882cb0d8bd2f9969f1560a6b6d16
kateryna <kateryna_neholiuk@ukr.net>
2020-12-11 16:13:37 +0200

    task welcome

---
+++ js/welcome.js
@@ -0,0 +1,24 @@
+const greet = language => {
+    let languages = {
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso',
+  default: 'Welcome'
+    }
+  return language in languages ? languages[language] : languages.default;    
+}
+greet('dutch'); //'Welkom'
+greet('IP_ADDRESS_INVALID'); //'Welcome'
