e23d92205deac6892b07e2da12e1752323be5b19
kateryna <kateryna_neholiuk@ukr.net>
2020-12-11 17:51:31 +0200

    task Remove String Spaces

---
+++ js/removeStringSpace.js
@@ -0,0 +1,5 @@
+function noSpace(x){
+    return x.replace(/\s+/g, '');
+    }
+noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'); //'88Bifk8hB8BB8BBBB888chl8BhBfd'
+noSpace('8aaaaa dddd r     '); //'8aaaaaddddr'
