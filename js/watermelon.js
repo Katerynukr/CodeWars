55b627a30a8d6c07baf0680d1735d1de32f2e994
kateryna <kateryna_neholiuk@ukr.net>
2020-12-11 14:52:52 +0200

    task watermelon

---
+++ js/watermelon.js
@@ -0,0 +1,12 @@
+function divide(weight){
+    if(weight === 2)
+    { ;return false}
+    if( weight % 2 === 0){
+      return true;
+    } else {
+      return false;}
+  }
+
+divide(100); // true
+divide(67); //false
+divide(90); //true
