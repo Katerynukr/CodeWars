23a1b76abb4949e6f2fd3427078a842db7623943
kateryna <kateryna_neholiuk@ukr.net>
2020-12-11 16:10:22 +0200

    task Grasshopper - Create the rooms

---
+++ js/grasshoperCreateRoom.js
@@ -0,0 +1,18 @@
+// Add rooms here
+var rooms = {
+    'name': {
+      'property':1,
+      'prop2':1,
+      'prop3':1
+    },
+     'description': {
+      'property':1,
+      'prop2':1,
+      'prop3':1
+    }, 
+      'completed': {
+      'property':1,
+      'prop2':1,
+      'prop3':1
+    }
+}
