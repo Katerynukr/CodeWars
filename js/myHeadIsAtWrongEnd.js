c4e536c7334ff6095ec9e1e68144c90229a0d654
kateryna <kateryna_neholiuk@ukr.net>
2020-12-11 15:15:52 +0200

    task My head is at the wrong end!

---
+++ js/myHeadIsAtWrongEnd.js
@@ -0,0 +1,7 @@
+function fixTheMeerkat(arr) {
+    let array = [arr[2], arr[1], arr[0]];
+     return array;
+   }
+
+fixTheMeerkat(["tails", "body", "heads"]); //["heads", "body", "tails"]
+fixTheMeerkat(["bottom", "middle", "top"]); //["top", "middle", "bottom"]
